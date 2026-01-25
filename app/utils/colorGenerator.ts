21fab7d329c1acd3aa49a76756689b0e2c4287af
Mish Wilson <mishwilsonk@gmail.com>
2026-01-25 10:53:23 -0500

    feat: color generating functions

---
+++ app/utils/colorGenerator.ts
@@ -0,0 +1,28 @@
+import type { Color } from "@/types/palette";
+
+// Needed: genrandColor, genrandompalettte, genpalettewith locks
+
+
+export function generateRandomColor(): string {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
+export function generatePalette(): Color[] {
+    return Array.from({length: 5}, () => ({
+        hex: generateRandomColor(),
+        locked: false
+    }))
+} 
+
+export function regenerateWithLocks(currentPalette: Color[]): Color[] {
+    return currentPalette.map(color => 
+        color.locked 
+        ? color
+        : { hex: generateRandomColor(), locked: false }
+    ) 
+} 
+
+// Validate hex Color Format - # [0-9A-F]x6
+export function isValidHex(hex: string) : boolean {
+    return /^#[0-9A-F]{6}$/i.test(hex)
+}
