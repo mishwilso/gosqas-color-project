c1ee92caf29b404d1c3c893027772ccfe0620dcf
Mish Wilson <mishwilsonk@gmail.com>
2026-01-25 13:11:55 -0500

    [feat]: Added Post and Delete for palette table

--- app/server/api/palettes/[id].delete.ts
+++ app/server/api/palettes/[id].delete.ts
@@ -1,1 +1,32 @@
 // DELETE palette by ID
+import { defineEventHandler, getRouterParams } from 'h3';
+import { tableClient } from "@/utils/azureStorage";
+import { createError } from 'h3';
+
+export default defineEventHandler(async (e) => {
+    try {
+
+        // grabs context
+        const id = getRouterParams(e).id;
+
+        // if no id - give an error
+        if (!id) {
+            throw createError({
+                statusCode: 400,
+                message: 'Palette ID is required'
+            })
+        }
+
+        // else delete id
+        await tableClient.deleteEntity('palettes', id);
+
+        // let them know it worked :)
+        return { success: true }
+
+    } catch (error : any){
+        throw createError({
+            statusCode: error.statusCode || 500,
+            message: error.message || 'Failed to delete palette'
+        })
+    }
+})
--- app/server/api/palettes/index.post.ts
+++ app/server/api/palettes/index.post.ts
@@ -1,1 +1,50 @@
 // POST new palette
+import { defineEventHandler, readBody } from 'h3';
+import { tableClient } from "@/utils/azureStorage";
+import { createError } from 'h3';
+import type { Palette, PaletteEntity } from "@/types/palette";
+
+export default defineEventHandler(async (e) => {
+    try {
+        const entry = await readBody(e);
+        const { name, colors } = entry;
+
+        if (!name || !colors || !Array.isArray(colors)) {
+            throw createError({
+                statusCode: 400,
+                message: 'Invalid palette data'
+            });
+        }
+
+        const id = crypto.randomUUID();
+        const createdAt = new Date().toISOString();
+
+        // Create entity to give to azurite table
+        const entity: PaletteEntity = {
+            partitionKey: 'palettes',
+            rowKey: id,
+            name,
+            colors: JSON.stringify(colors),
+            createdAt
+        }
+
+        await tableClient.createEntity(entity);
+
+
+        // create and return the palette we added (for validation)
+        const palette: Palette = {
+            id,
+            name,
+            colors,
+            createdAt
+        }
+
+        return palette
+
+    } catch (error : any) {
+        throw createError({
+            statusCode: error.statusCode || 500,
+            message: error.message || "Failed to create palette"
+        });
+    } 
+})
